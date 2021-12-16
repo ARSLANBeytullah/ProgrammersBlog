1206ff41d36a2e2378ef81b1e98a8315c1f53042
ARSLANBeytullah <arslanbeytullah@gmail.com>
2021-12-16 18:20:54 +0300

    EntityBase,IEntity,Category classes are coded

---
+++ ProgrammerBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    //Base değerlerin diğer sınıflarda ddeğişikliğe uğramasını isteyebiliriz. O yüzden abstract ile imzaladık. 
+    //Abstract sayesinde override işlemi yapabiliriz. Override edilmiş özellikleri de virtual olarak tanımlamamız gerekiyor
+    public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now; //Oluşturma tarihi değiştirilebileceğindenc virtual olarak işaretledim.
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string  CreatedByName { get; set; }
+        public virtual string ModifiedByName { get; set; }
+    }
+}
