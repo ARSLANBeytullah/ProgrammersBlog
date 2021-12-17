37b5f76aec6147034988260180c94b153d6ae122
ARSLANBeytullah <arslanbeytullah@gmail.com>
2021-12-17 16:42:45 +0300

    Added IUnitofWork,UnitOfWork

---
+++ ProgrammerBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerBlog.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable //Unit of work tasarım deseni sayesinde bütün repositoryilerimizi tek bir noktadan yönetebiliriz.
+    {
+        IArticleRepository Articles { get; } //unitofwork.Articles diyerek Article'a erişebiliyor olacağız.
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; } //_unitOfWork.Categories.AddAsync(); bu şekilde kullancağız.
+
+
+        //_unitOfWork.Categories.AddAsync(category);
+        //_unitOfWork.Users.AddAsync(user);
+        //_unitOfWork.SaveAsync();           bu şekilde kategori ve kullanıcı ekledikten sonra SaveAsync ile ekleme işlemi yapacağım. Eğer bu iki methodtan birisi bile hatalı çalışırsa herhangi bir işlemi yapmayacak ve oluşabilecek hataları bu şekilde ortadan kaldırmış olacağız.
+       Task<int> SaveAsync(); //Veritabanın da birçok işlemi yaptıkdan sonra asenkron bir şekilde save işlemi yapacağız.
+    }
+}
---
+++ ProgrammerBlog.Data/Concrete/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using ProgrammerBlog.Data.Abstract;
+using ProgrammerBlog.Data.Concrete.EntityFramework.Contexts;
+using ProgrammerBlog.Data.Concrete.EntityFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerBlog.Data.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ProgrammersBlogContext _context;
+        private EfArticleRepository _articleRepository;
+        private EfCategoryRepository _categoryRepository;
+        private EfCommentRepository _commentRepository;
+        private EfRoleRepositoy _roleRepository;
+        private EfUserRepository _userRepository;
+
+        public UnitOfWork(ProgrammersBlogContext context) //Dependies injection
+        {
+            _context = context;
+        }
+
+
+
+        public IArticleRepository Articles => _articleRepository ?? new EfArticleRepository(_context); //Article.AddAsync denirse ona _articleRepository dönülür. Anca AddAsync değilde null bir değer dönülürse eğer o zaman EfArticleRepository'i newleyerek kullanıcıya gönderiyoruz.
+
+        public ICategoryRepository Categories => _categoryRepository ?? new EfCategoryRepository(_context); //Null Coalescing Operatörü(??) bu operatör sayesinde bir değişkenin değerinin null olduğu durumda alternatif değer döndürebiliriz.
+
+        public ICommentRepository Comments => _commentRepository ?? new EfCommentRepository(_context);
+
+        public IRoleRepository Roles => _roleRepository ?? new EfRoleRepositoy(_context);
+
+        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
+
+       
+
+        public async Task<int> SaveAsync() //Bu method bizim contextimizin savechanges methoduna eşit olacak
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
