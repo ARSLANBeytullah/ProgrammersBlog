8c26e5a324d49a79bbedebac8fd607eb32ecb43e
ARSLANBeytullah <arslanbeytullah@gmail.com>
2021-12-17 14:09:24 +0300

    Added ArticleMap and CategoryMap

---
+++ ProgrammerBlog.Data/Concrete/EntityFramework/Mappings/ArticleMap.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class ArticleMap : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.HasKey(a => a.Id); //a = article demektir. Key'i id olarak tanımladım.
+            builder.Property(a => a.Id).ValueGeneratedOnAdd(); //Bu identity yapar. ve id birer birer artar.
+            builder.Property(a => a.Title).HasMaxLength(100);
+            builder.Property(a => a.Title).IsRequired(true);  //Zorunlu alan. Defaultu zaten truedur. True yazmaksak da olur
+            builder.Property(a => a.Content).IsRequired();
+            builder.Property(a => a.Content).HasColumnType("NVARCHAR(MAX)");
+            builder.Property(a => a.Date).IsRequired();
+            builder.Property(a => a.SeoAuthor).IsRequired();
+            builder.Property(a => a.SeoAuthor).HasMaxLength(50);
+            builder.Property(a => a.SeoDescription).HasMaxLength(100);
+            builder.Property(a => a.SeoDescription).IsRequired();
+            builder.Property(a => a.SeoTags).IsRequired();
+            builder.Property(a => a.SeoTags).HasMaxLength(50);
+            builder.Property(a => a.ViewsCount).IsRequired();
+            builder.Property(a => a.CommentCount).IsRequired();
+            builder.Property(a => a.Thumbnail).IsRequired();
+            builder.Property(a => a.Thumbnail).HasMaxLength(250);
+            builder.Property(a => a.CreatedByName).IsRequired();
+            builder.Property(a => a.CreatedByName).HasMaxLength(50);
+            builder.Property(a => a.ModifiedByName).IsRequired();
+            builder.Property(a => a.ModifiedByName).HasMaxLength(50);
+            builder.Property(a => a.CreatedDate).IsRequired();
+            builder.Property(a => a.ModifiedDate).IsRequired();
+            builder.Property(a => a.IsActive).IsRequired();
+            builder.Property(a => a.IsDeleted).IsRequired();
+            builder.Property(a => a.Note).HasMaxLength(500);
+            builder.HasOne<Category>(a => a.Category).WithMany(c => c.Articles).HasForeignKey(a 
+                => a.CategoryId);  //Bir Categoryde birden çok makale olabilir. ve categoryid bir foreign keydir.
+            builder.HasOne<User>(u => u.User).WithMany(u => u.Articles).HasForeignKey(a => a.UserId);
+            builder.ToTable("Articles");
+        }
+    }
+}
